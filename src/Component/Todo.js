2a919e344be947d8f83f22ac2001e6b5a583b350
Anhtuvu24 <anhtuvu2413@gmail.com>
2022-08-19 16:11:59 +0700

    TodoList-19-8

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -19,6 +19,12 @@ class Todo extends React.Component {
         this.setState({editing: true})
     };
 
+    handleUpdate = e => {
+        if(e.key === "Enter") {
+            this.setState({editing: false});
+        }
+    };
+
     render() {
         let viewMode = {};
         let editMode = {};
@@ -32,10 +38,10 @@ class Todo extends React.Component {
             <div className='todo-container'>
                 <input type = "checkbox" onChange={() => this.props.handleCheckBox(this.props.id)} checked = {this.props.isCompleted}/>
                 <li className = {this.props.isCompleted? "completed" : ''}>
-                    <div style = {viewMode} onDoubleClick={this.handleEditing}>
+                    <div className='todoDisplay' style = {viewMode} onDoubleClick={this.handleEditing}>
                         {this.props.index}. {this.props.name}
                     </div>
-                    <input style={editMode} type = "text"/>
+                    <input onChange={e => {this.props.setUpdate(e.target.value, this.props.id)}} onKeyDown = {this.handleUpdate} style={editMode} type = "text"/>
                 </li>
                 <button onClick={this.handleRemoveTodo}>delelte</button>
             </div>
@@ -44,4 +50,3 @@ class Todo extends React.Component {
 }
 
 export default Todo;
-
